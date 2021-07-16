71b23be67561fbf1e8b1631b7faf457421f1df17
zjh369258 <78715383+zjh369258@users.noreply.github.com>
2021-07-16 10:06:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,15 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# 删除老argon
+rm -rf package/lean/luci-theme-argon
+# 拉取argon主题
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+git clone  https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+
+# 拉取微信推送、京东签到插件
+#git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
+#git clone https://github.com/jerrykuku/node-request.git package/lean/node-request
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus
