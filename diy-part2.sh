f4f0955426f1dc71335f38f225591297fd6c0851
zjh369258 <78715383+zjh369258@users.noreply.github.com>
2022-05-27 06:42:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,18 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#修改主机名
+sed -i 's/OpenWrt/HC5661/g' package/base-files/files/bin/config_generate
+
+#切换ramips内核到5.10
+sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.10' target/linux/ramips/Makefile
+
+#设置WIFI
+sed -i 's/OpenWrt/HIWIFI-0AE7EA/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '/encryption/a\set wireless.default_radio${devidx}.key=coolxiaomi' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#修改登录密码为coolxiaomi
+#sed -i '/root/croot:$1$CBd7u73H$LvSDVXLBrzpk4JfuuN.Lv1:18676:0:99999:7:::' package/base-files/files/etc/shadow
 # 删除老argon
 #rm -rf package/lean/luci-theme-argon
 # 拉取argon主题
